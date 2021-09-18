825bf31ed991ee8199f90d8c429f755cd3c34e03
abrahamvera@gmail.com <Program2018>
2021-09-18 10:14:21 -0400

    Added Router and Switch

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
@@ -48,17 +48,27 @@ class App extends Component {
         <Navbar />
         <div className="container">
           <Alert alert={this.state.alert} />
-          <Search 
-            searchUsers={this.searchUsers} 
-            clearUsers={this.clearUsers} 
-            showClear={ this.state.users.length > 0 ? true : false }
-            setAlert={this.setAlert}
-            />
-          <Users loading={this.state.loading} users={this.state.users}/>
+          
+          <Switch>
+            <Route exact path='/' render={props =>(
+              <Fragment>
+                <Search 
+                  searchUsers={this.searchUsers} 
+                  clearUsers={this.clearUsers} 
+                  showClear={ this.state.users.length > 0 ? true : false }
+                  setAlert={this.setAlert}
+                  />
+                <Users loading={this.state.loading} users={this.state.users}/>
+              </Fragment>
+            )} />
+
+          </Switch>
+
+
         </div>
       </div>
       </Router>
-      
+
     );
   }
 }
