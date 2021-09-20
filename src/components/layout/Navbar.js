047f893940c6311d2760c5541c5565f5012cc338
abrahamvera@gmail.com <Program2018>
2021-09-19 22:51:05 -0400

    Added Router

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export class Navbar extends Component {
   static defaultProps = {
@@ -13,13 +14,19 @@ export class Navbar extends Component {
   };
 
   render() {
-    const varName = 'Abes Variable';
     return (
       <nav className="navbar bg-primary">
         <h1>
           <i className={this.props.icon} /> {this.props.title}
         </h1>
-        <h2>{varName}</h2>
+        <ul>
+          <li>
+            <Link to='/' >Home</Link>
+          </li>
+          <li>
+            <Link to='/about' >About</Link>
+          </li>
+        </ul>
       </nav>
     );
   }
